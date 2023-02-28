396c7c9bd45b664197f9962adb4655042f6b66a4
yeori <yeori.seo@gmail.com>
2023-02-28 17:40:00 +0900

    화면

--- src/components/layout/LayoutWrapper.vue
+++ src/components/layout/LayoutWrapper.vue
@@ -1,11 +1,26 @@
 <template>
-  <div class="layout-h"><slot /></div>
+  <div class="layout-h" :style="`padding-top: ${styles.topPadding}px`">
+    <slot />
+  </div>
 </template>
 
 <script>
+import { reactive, watch } from "vue";
+import { useRoute } from "vue-router";
+
 export default {
   setup() {
-    return {};
+    const route = useRoute();
+    const styles = reactive({ topPadding: 0 });
+    const updatePadding = () => {
+      const { path } = route;
+      const padding = path.startsWith("/quiz") ? 16 : 0;
+      styles.topPadding = padding;
+    };
+    watch(route, () => {
+      setTimeout(updatePadding);
+    });
+    return { styles };
   },
 };
 </script>
--- src/views/user/WelcomeJoin.vue
+++ src/views/user/WelcomeJoin.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="welcome">
     <Logo :imagePath="membership.image" size="md" />
-    <h3>계정을 생성했습니다.</h3>
+    <h3>환영합니다.</h3>
+    <ParaText
+      >로그인 후 이용할 수 있으며 7일간의 무료체험이 제공됩니다.</ParaText
+    >
     <router-link to="/">로그인 페이지로 이동</router-link>
   </div>
 </template>
@@ -9,9 +12,11 @@
 <script>
 import Logo from "@/components/oauth/Logo.vue";
 import { useStore } from "vuex";
+import { ParaText } from "../../components/text";
 export default {
   components: {
     Logo,
+    ParaText,
   },
   setup() {
     const store = useStore();
